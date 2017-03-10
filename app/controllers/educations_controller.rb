bbb1463a5f94102837d0579713bf40715c9d8a1e
Gedeon Ntwali <ntwari28@gmail.com>
2017-03-10 15:04:21 +0200

    I worked on education controller

---
+++ app/controllers/educations_controller.rb
@@ -0,0 +1,15 @@
+class EducationsController < ApplicationController
+
+   def show
+    @education = Unirest.get("http://localhost:3000/students/#{params[:id]}.json").body
+  end
+  def edit
+    @education = Unirest.get("http://localhost:3000/students/#{params[:id]}.json").body
+  end
+
+  def update
+    @education = Unirest.patch("http://localhost:3000/students/#{params[:id]}.json", :parameters => {:start_date=> params[:start_date], :end_date=> params[:end_date], :degree=> params[:degree],:university_name => params[:university_name], :detail=> params[:detail]}).body
+
+    return Education.new(education_hash)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -7,5 +7,9 @@ Rails.application.routes.draw do
   get "/students/:id", to: 'students#show'
   get "/students/:id/edit", to: 'students#edit'
   patch "/students/:id", to: 'students#update'
+
+  get "/educations/:id", to: 'educations#show'
+  get "/educations/:id/edit", to: 'educations#edit'
+  patch "/educations/:id", to: 'educations#update'
   
 end
