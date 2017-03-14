166c916ec7f1ca2eb11b19528b75e88649f938c2
Moise Rwibutso <moiseri01@gmail.com>
2017-03-14 13:49:49 +0200

    Have created experience feature

---
+++ app/controllers/experiences_controller.rb
@@ -0,0 +1,19 @@
+class ExperiencesController < ApplicationController
+
+  def index 
+    @experiences = Experience.all
+  end
+
+  def show
+   @experience = Unirest.get("http://localhost:3000/experiences/#{params[:id]}.json"
+  end
+
+  def edit 
+    @experience = Unirest.get("http://localhost:3000/experiences/#{params[:id]}.json")
+  end
+
+  def update
+    @experience = Unirest.patch("http://localhost:3000/experiences/#{params[:id]}.json",:parameters => {:id=> params[:id], :start_date=> params[:start_date], :degree=> params[:degree],:university_name=> params[:university_name], :details => params[:details]}).body
+    return Experience.new(experience_hash)
+  end
+end
