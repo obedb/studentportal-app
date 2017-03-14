be7dc189949372b179b0a3a61e063299c33d4476
Gedeon Ntwali <ntwari28@gmail.com>
2017-03-14 10:58:17 +0200

    Worked on index

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   post "/login", to: 'sessions#create'
   get "/logout", to: 'sessions#destroy'
 
+  get "/", to: 'students#index'
   get "/students/:id", to: 'students#show'
   get "/students/:id/edit", to: 'students#edit'
   patch "/students/:id", to: 'students#update'
