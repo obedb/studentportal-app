c68d9fa3f365b4bddcacfcfb4e08c190524a9558
Obedb <obedbigirimana@gmail.com>
2017-03-15 11:54:35 +0200

    created update action

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -16,6 +16,6 @@ class StudentsController < ApplicationController
   def update
     @student = Unirest.patch("http://localhost:3000/api/v2/students/#{params[:id]}.json", :parameters => {:id=> params[:id], :first_name=> params[:first_name], :last_name=> params[:last_name],:email => params[:email], :phone_number=> params[:phone_number],:short_bio=> params[:short_bio], :linkedin_url=> params[:linkedin_url], :twitter_handle=> params[:twitter_handle], :blog_url=> params[:blog_url], :resume_url=> params[:resume_url], :github_url=> params[:github_url], :photo=> params[:photo]}).body
 
-    return Student.new(student_hash)
+      redirect_to "/students/#{@student["id"]}"
   end
 end
