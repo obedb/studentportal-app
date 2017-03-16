71cda3caa3c86a712d0c989d2ed1eca423afb8d6
Obedb <obedbigirimana@gmail.com>
2017-03-16 11:54:43 +0200

    worked on sessions controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,23 +5,28 @@ class SessionsController < ApplicationController
 
   def create
     @students = Unirest.get("http://localhost:3000/api/v2/students.json").body
-    student = Student.find_by(email: params[:email])
-    if student && student.authenticate(params[:password])
-      flash[:success] = 'Successfully logged in!'
-      redirect_to '/'
-    else
-      flash[:warning] = 'Invalid email or password!'
-      redirect_to '/login'
-    end
+    @students.each do |student|
+      student 
+
+      if params[:email] == student["email"]
+        flash[:success] = 'Successfully logged in!'
+        redirect_to '/'
+      else
+        flash[:warning] = 'Invalid email or password!'
+        redirect_to '/login'
+      end
+   
   end
+
 
   def destroy
     session[:user_id] = nil
     flash[:success] = 'Successfully logged out!'
     redirect_to '/login'
+
   end
 end
-
+end
 
 
 
